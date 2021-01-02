f97a7580e895692f1d6feee7e9df842c8144da26
liboxiang <1169778535@qq.com>
2021-01-02 22:09:48 +0800

    【新增】+ example、publish：新增example和publish信息

--- README.md
+++ README.md
@@ -1,14 +1,41 @@
+[dart pub](https://pub.dev/packages/list_index_manager)
 # list_index_manager
 
-A new Flutter package project.
+显示列表数据的时候经常会遇到是需要隐藏部分数据的情况，list_index_manager可以将隐藏部分数据后的数组下标转换成原始数据下标。
 
-## Getting Started
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+![原理图](./read_me_image/list_index_manager.png)
+## 用法
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+```
+List<Info> dataList = [];
+ListIndexManager manager = ListIndexManager();
+
+ListView.builder(
+        itemCount: dataList.length - manager.totalHideNumber,
+        itemBuilder: (_, index) {
+          Info info = dataList[manager.indexOf(index)];
+          ...
+        },
+      )
+```
+### 隐藏数据
+```
+//隐藏dataList中index下标数据
+manager.hide(index);
+//隐藏dataList中下标从start到end的数据
+manager.hideRange(start,end);
+```
+
+### 显示数据
+```
+//隐藏dataList中index下标数据
+manager.show(index);
+//隐藏dataList中下标从start到end的数据
+manager.showRange(start,end);
+```
+
+### 清除所有设置
+```
+manager.clear();
+```
---
+++ example/lib/list_index_manager_check.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:list_index_manager/list_index_manager.dart';
+
+class ListIndexManagerCheckPage extends StatefulWidget {
+  @override
+  _ListIndexManagerCheckPageState createState() => _ListIndexManagerCheckPageState();
+}
+
+class _ListIndexManagerCheckPageState extends State<ListIndexManagerCheckPage> {
+  List<Info> dataList = [];
+  ListIndexManager manager = ListIndexManager();
+  @override
+  void initState() {
+    super.initState();
+    List.generate(5, (index) {
+      dataList.add(Info(groupId: 'one', value: index, isFirst: index == 0));
+    });
+
+    List.generate(8, (index) {
+      dataList.add(Info(groupId: 'two', value: index + 5, isFirst: index == 0));
+    });
+
+    List.generate(3, (index) {
+      dataList.add(Info(groupId: 'three', value: index + 13, isFirst: index == 0));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('测试'),
+      ),
+      body: ListView.builder(
+        itemCount: dataList.length - manager.totalHideNumber,
+        itemBuilder: (_, index) {
+          Info info = dataList[manager.indexOf(index)];
+          if (info.isFirst) {
+            return Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    int start;
+                    int end;
+                    if (info.groupId == 'one') {
+                      start = 1;
+                      end = 4;
+                    } else if (info.groupId == 'two') {
+                      start = 6;
+                      end = 12;
+                    } else if (info.groupId == 'three') {
+                      start = 14;
+                      end = 15;
+                    }
+                    setState(() {
+                      if (info.isOpen) {
+                        manager.hideRange(start, end);
+                      } else {
+                        manager.showRange(start, end);
+                      }
+                      info.isOpen = !info.isOpen;
+                    });
+                  },
+                  child: Container(
+                    height: 44,
+                    width: BoxConstraints.expand().maxWidth,
+                    color: Colors.red,
+                    child: Text(
+                      info.groupId,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                if (info.isOpen)
+                  ListTile(
+                    title: Text(info.groupId),
+                    subtitle: Text(info.value.toString()),
+                  )
+              ],
+            );
+          }
+          return ListTile(
+            title: Text(info.groupId),
+            subtitle: Text(info.value.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Info {
+  final String groupId;
+  final int value;
+  bool isOpen = true;
+  final bool isFirst;
+  Info({this.groupId, this.value, this.isFirst = false});
+}
